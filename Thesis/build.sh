b9708b805b9fa05bcdd650e141e00b366e2e56e5
Rafal_Sztandera <rsztandera@mt-silesia.com>
2018-06-09 16:26:17 +0200

    bionic build

--- Thesis/build.sh
+++ Thesis/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-texliveonfly -c lualatex main.tex | grep Warning
+texliveonfly -c xelatex main.tex | grep Warning
 bibtex main.aux
-texliveonfly -c lualatex main.tex | grep Warning
-texliveonfly -c lualatex main.tex | grep Warning
+texliveonfly -c xelatex main.tex | grep Warning
+texliveonfly -c xelatex main.tex | grep Warning
